cb3e81fc193cf4d4edc21c9b99c91315f1e1707a
Justin <me@justinmaslin.com>
2015-12-12 16:32:28 -0500

    Cleanup file

--- src/fetcher.js
+++ src/fetcher.js
@@ -8,34 +8,12 @@ class Fetcher {
 	}
 
 	fetch() {
+		console.log("Fetching...");
 		var requester = new RequestBuilder(this.date);
 		var executer = new RequestExecuter(requester.buildRequest());
 		return executer.sendRequest();
-		// console.log("Fetching...");
 	}
 
 }
 
 exports.Fetcher = Fetcher;
-
-/*
-
-
-Functions:
-
-- Given a list of dates
-- Build a request
-- Execute the request
-- Return the data
-
-Objects:
-
-RequestBuilder
-e.g buildUrl - Given a date, generate query string (return url)
-
-RequestExecuter
-- Reads in RequestBuilder object, makes request, returns data
-- Can use promise library
-- Get a good http library with promises (or q
-
-*/
