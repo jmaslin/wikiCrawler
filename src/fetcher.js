09ec6ce5616bc7582d8d3e676c2ee202169d65d5
Justin <me@justinmaslin.com>
2015-12-12 15:26:26 -0500

    Update base fetcher class

--- src/fetcher.js
+++ src/fetcher.js
@@ -1,7 +1,17 @@
+var RequestBuilder = require('./requestBuilder').RequestBuilder;
+var RequestExecuter = require('./requestExecuter').RequestExecuter;
+
 class Fetcher {
 
+	constructor(date) {
+		this.date = date;
+	}
+
 	fetch() {
-		console.log("Fetching...");
+		var requester = new RequestBuilder(this.date);
+		var executer = new RequestExecuter(requester.buildRequest());
+		return executer.sendRequest();
+		// console.log("Fetching...");
 	}
 
 }
--- test/fetcher.spec.js
+++ test/fetcher.spec.js
@@ -1,20 +1,34 @@
-var assert = require('assert');
 var expect = require('expect.js');
+var moment = require('moment');
 
 var Fetcher = require('../build/fetcher.js').Fetcher;
 
 describe('Fetcher', function () {
 	
+	var date = moment().month('October').date(3);
+	var fetcher = new Fetcher(date);
+
 	it('exists', function () {
 		expect(new Fetcher()).not.to.be(null);
 	});
 
 	describe('fetch', function () {
 
-		var fetcher = new Fetcher('');
+		it('returns data', function (done) {
 
-		it('returns a url', function () {
-			
+			var expected = [
+				'title',
+				'pageId',
+				'text'
+			];
+
+			fetcher.fetch()
+				.then(function (response) {
+					expect(response.data).to.only.have.keys(expected);
+					
+					done();
+				});
+
 		});
 
 	});
