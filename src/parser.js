8b4b60ddf9d09ef7d03a5503c8e2c710a767fbae
Justin <me@justinmaslin.com>
2015-12-12 16:32:39 -0500

    Fix bug with name text parsing

--- src/parser.js
+++ src/parser.js
@@ -29,24 +29,32 @@ class Parser {
 	parseListItem(index, element) {
 
 		var $ = cheerio.load(element);
-
 		var item = {};
+
+		var yearText, yearURI, nameText, nameURI, nameArray;
 
 		var listItemText = $(element).text();
 
-		var year = listItemText.substring(0, listItemText.indexOf('\u2013')).trim();
-		var name = listItemText.substring(listItemText.indexOf('\u2013')+2, listItemText.indexOf(','))
+		yearText = listItemText.substring(0, listItemText.indexOf('\u2013')).trim();
 
-		var nameURI = $('a:contains('+name+')').attr('href');
-		var yearURI = $('a:contains('+year+')').attr('href');
+		if (listItemText.indexOf(',') === -1) {
+			nameText = listItemText.substring(listItemText.indexOf('\u2013')+2, listItemText.length);
+		} else {
+			nameText = listItemText.substring(listItemText.indexOf('\u2013')+2, listItemText.indexOf(','));
+		}
 
+		nameArray = nameText.split(' ');
+
+		nameURI = $('a:contains('+nameArray[0]+')').attr('href');
+		yearURI = $('a:contains('+yearText+')').attr('href');
+		
 		item.year = {
-			text: year,
+			text: yearText,
 			uri: yearURI
 		};
 
 		item.name = {
-			text: name,
+			text: nameText,
 			uri: nameURI
 		};
 
