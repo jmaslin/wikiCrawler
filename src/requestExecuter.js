adcee95e68ee108527ff848b9139438d82910031
Justin <me@justinmaslin.com>
2015-12-12 15:26:52 -0500

    Init requestExecuter

---
+++ src/requestExecuter.js
@@ -0,0 +1,30 @@
+var axios = require('axios');
+
+axios.interceptors.response.use(function (response) {
+	response.data = {
+		title: response.data.parse.title,
+		pageId: response.data.parse.pageid,
+		text: response.data.parse.text['*']
+	};
+  return response;
+}, function (error) {
+	return Promise.reject(error);
+});
+
+class RequestExecuter {
+
+	constructor(requestObject) {
+		this.requestObject = requestObject;
+	}
+
+	buildRequest() {
+		return axios(this.requestObject);
+	}
+
+	sendRequest() {
+		return this.buildRequest();
+	}
+
+}
+
+exports.RequestExecuter = RequestExecuter;
