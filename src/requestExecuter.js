fc12cf0f1ce7a03e3db8109494bcff8260974354
Justin <me@justinmaslin.com>
2015-12-16 00:03:37 -0500

    Use any interceptor depending on request type, update test structure to be support future types

--- src/requestExecuter.js
+++ src/requestExecuter.js
@@ -1,24 +1,13 @@
 var axios = require('axios');
 
-axios.interceptors.response.use(function (response) {
-  response.data = {
-    title: response.data.parse.title,
-    pageId: response.data.parse.pageid,
-    text: response.data.parse.text['*']
-  };
-  return response;
-}, function (error) {
-  return Promise.reject(error);
-});
-
 class RequestExecuter {
 
   constructor(requestObject) {
-    this.requestObject = requestObject;
+    this._requestObject = requestObject;
   }
 
   buildRequest() {
-    return axios(this.requestObject);
+    return axios(this._requestObject);
   }
 
   sendRequest() {
--- test/requestExecuter.spec.js
+++ test/requestExecuter.spec.js
@@ -9,77 +9,87 @@ var fs = require('fs');
 
 var RequestExecuter = require('../build/requestExecuter.js').RequestExecuter;
 
+var wikipedia = nock('https://en.wikipedia.org').log(console.log);
+nock.disableNetConnect();      
+
 describe('RequestExecuter', function () {
 
-  var wikipedia = nock('https://en.wikipedia.org').log(console.log);
-
-  nock.disableNetConnect();
-
-  var requestObject = {
-    url: 'https://en.wikipedia.org/w/api.php',
-    params: {
-      action: 'parse',
-      prop: 'text',
-      section: 2,
-      format: 'json',
-      page: 'October_3'         
-    }
-  };
-
-  var expectedResponse = JSON.parse(fs.readFileSync('test/data/day-response.json', 'utf8'));
-
-  var executer = new RequestExecuter(requestObject);
-  
   it('exists', function () {
     expect(new RequestExecuter()).to.be.a('object');
   });
 
-  describe('buildRequest', function () {
-    it('creates the request promise', function () {
-      expect(executer.buildRequest().then).to.be.a('function');
-    });
-  });
+  describe('list request', function () {
 
-  describe('sendRequest', function () {
+    var requestObject = {
+      url: 'https://en.wikipedia.org/w/api.php',
+      params: {
+        action: 'parse',
+        prop: 'text',
+        format: 'json',
+        section: 2,
+        page: 'October_3'         
+      },
+      transformResponse: [function (data) {
+        data = JSON.parse(data);
+        return {
+          title: data.parse.title,
+          pageId: data.parse.pageid,
+          text: data.parse.text['*']
+        };
+      }]
+    };
 
-    var wiki = nock('https://en.wikipedia.org')
-                .persist()
-                .get('/w/api.php?'+querystring.stringify(requestObject.params))
-                .reply(200, expectedResponse);
+    var expectedResponse = JSON.parse(fs.readFileSync('test/data/day-response.json', 'utf8'));
+    var executer = new RequestExecuter(requestObject);
 
-    it('returns a status of 200', function () {
-      var response = executer.sendRequest()
-        .then(function (response) {
-          return response.status;
-        });
-
-      return expect(response).to.eventually.equal(200);
-    });
-
-    it('returns the proper format', function () {
-
-      var expected = [
-        'title',
-        'pageId',
-        'text'
-      ];
-
-      var response =  executer.sendRequest()
-        .then(function (response) {
-          return response.data;
-        });
-      
-      return expect(response).to.eventually.have.all.keys(expected);
+    describe('buildRequest', function () {
+      it('creates the request promise', function () {
+        expect(executer.buildRequest().then).to.be.a('function');
+      });
     });
 
-    it('returns the correct title', function () {
+    describe('sendRequest', function () {
 
-      var response = executer.sendRequest()
-        .then(function (response) {
-          return response.data.title;
-        });
+      var wiki = nock('https://en.wikipedia.org')
+                  .persist()
+                  .get('/w/api.php?'+querystring.stringify(requestObject.params))
+                  .reply(200, expectedResponse);
 
-      return expect(response).to.eventually.eql(expectedResponse.parse.title);
+      it('returns a status of 200', function () {
+        var response = executer.sendRequest()
+          .then(function (response) {
+            return response.status;
+          });
+
+        return expect(response).to.eventually.equal(200);
+      });
+
+      it('returns the proper format', function () {
+
+        var expected = [
+          'title',
+          'pageId',
+          'text'
+        ];
+
+        var response =  executer.sendRequest()
+          .then(function (response) {
+            return response.data;
+          });
+        
+        return expect(response).to.eventually.have.all.keys(expected);
+      });
+
+      it('returns the correct title', function () {
+
+        var response = executer.sendRequest()
+          .then(function (response) {
+            return response.data.title;
+          });
+
+        return expect(response).to.eventually.eql(expectedResponse.parse.title);
+      });
+
     });
 
   });
