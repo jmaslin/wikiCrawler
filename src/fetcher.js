a9323e57227cc9fb38cad1b6d0541678e7acdd77
Justin <me@justinmaslin.com>
2015-12-16 22:14:38 -0500

    Fetch person class

--- src/fetcher.js
+++ src/fetcher.js
@@ -1,4 +1,6 @@
 var ListRequestBuilder = require('./listRequestBuilder').ListRequestBuilder;
+var PersonRequestBuilder = require('./personRequestBuilder').PersonRequestBuilder;
+
 var RequestExecuter = require('./requestExecuter').RequestExecuter;
 
 class Fetcher {
@@ -7,8 +9,22 @@ class Fetcher {
     this._date = date;
   }
 
+  get date() {
+    return this._date;
+  }
+
+  set date(date) {
+    this._date = date;
+  }
+
   getList(listType) {
     var requester = new ListRequestBuilder(this._date, listType);
+    var executer = new RequestExecuter(requester.buildRequest());
+    return executer.sendRequest();
+  }
+
+  getPerson(person) {
+    var requester = new PersonRequestBuilder(person);
     var executer = new RequestExecuter(requester.buildRequest());
     return executer.sendRequest();
   }
