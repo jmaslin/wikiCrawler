f131b2fcf103bcddaf89731e50dca22d5f88583f
Justin <me@justinmaslin.com>
2016-01-09 16:36:15 -0500

    More efficient approach

--- src/index.js
+++ src/index.js
@@ -13,17 +13,23 @@ var listType = "births";
 var index = 0;
 
 // http://www.frontcoded.com/splitting-javascript-array-into-chunks.html
-var createGroupedArray = function(arr, chunkSize) {
+var createGroupedArray = function (arr, chunkSize) {
   var groups = [], i;
   for (i = 0; i < arr.length; i += chunkSize) {
-      groups.push(arr.slice(i, i + chunkSize));
+    groups.push(arr.slice(i, i + chunkSize));
   }
   return groups;
 }
 
-var buildPerson = function buildPerson(person, response) {
-  person.aboutText = response.extract;
-  person.pageId = response.pageid;
+var buildPerson = function buildPerson(person, personData) {
+
+  if (personData) {
+    person.aboutText = personData.extract;
+    person.pageId = personData.pageid;
+  } else {
+    console.log("No data for person: " + person.name);
+    person.error = "No data found.";
+  }
 
   return person;
 };
@@ -47,11 +53,40 @@ var printDate = function printDate (dayMoment) {
   console.log(dateFormatted);
 };
 
+var personListRequestSuccess = function personListRequestSuccess (group, response, monthDay) {
+
+  var deferred = Q.defer();
+
+  var responseData, groupKeys, people = [];
+
+  responseData = response.data.query.pages;
+  groupKeys = Object.keys(responseData);
+
+  if (groupKeys.length !== group.length) {
+    console.log("Not all data returned for query.");
+  }
+
+  group.map(function (person, index) {
+    var personData = responseData[ groupKeys[index] ];
+    person = buildPerson(person, personData);
+    person.monthDay = monthDay;
+
+    return person;
+  });
+
+  deferred.resolve(group);
+
+  return deferred.promise;
+};
+
+var responseError = function responseError (err) {
+  console.log("Error ", err);
+};
+
 var buildListForDay = function buildListForDay (dayMoment) {
 
   var dayDeferred = Q.defer();
   var monthDay = dayMoment.format('MM/DD');
-  var people = [];
 
   // Get list for day + parse it`
   var dayList = getListForDay(dayMoment);
@@ -61,53 +96,33 @@ var buildListForDay = function buildListForDay (dayMoment) {
     // Break list into chunks 
     var peopleGroups = createGroupedArray(peopleList, 20);
 
-    // console.log("Groups: " + peopleGroups.length);
-
     async.each(peopleGroups, function iterator(group, groupIndex, arr) {
       var deferred = Q.defer();
-
       var personList = fetcher.getPersonList(group);
+
       personList.then(function (response) {
-        var responseData, groupKeys;
-
-        responseData = response.data.query.pages;
-        groupKeys = Object.keys(responseData);
-
-        if (groupKeys.length !== group.length) {
-          console.log("Not all data returned for query. Group " + groupIndex);
-        }
-
-        group.forEach(function (person, index) {
-          var personData = responseData[ groupKeys[index] ];
-
-          if (personData) {
-            person = buildPerson(person, personData);
-          } else {
-            console.log("No data for person: " + person.name);
-            person.error = "No data found.";
-          }
-          
-          person.monthDay = monthDay;
-          people.push(person);
+        personListRequestSuccess(group, response, monthDay).then(function (group) {
+          deferred.resolve(group);
         });
-
-        // console.log("After group: " + groupIndex);
-
-        deferred.resolve();
-      }, function (err) {
-        console.log("Error: ", err);
-      });
+      }, responseError);
 
       return deferred.promise;
     })
     .then(function (results) {
+      var people = [];
+
+      results.forEach(function (group) {
+        group.forEach(function (person) {
+          people.push(person);
+        });
+      });
 
       console.timeEnd("Time to complete");
       console.log("Count: " + people.length);
 
       dayDeferred.resolve(people);
     }, function (err) {
-      console.log("Error ", err);
+      responseError(err);
       dayDeferred.resolve();
     })
     .done();
@@ -123,14 +138,9 @@ var start = moment('2016-01-01', 'YYYY-MM-DD');
 var end = moment('2016-12-31', 'YYYY-MM-DD');
 var range = moment.range(start, end);
 
-saver = new Saver('tmp/all-1.json');
 fetcher = new Fetcher();
 
-var dayPromises = [];
-var delay = 0, delayAmount = 1000;
-
 var days = [];
-
 range.by('days', function (dayMoment) {
   days.push(dayMoment);
 });
@@ -143,14 +153,14 @@ async.eachLimit(days, 2, function iterator(day, index, arr) {
   return buildListForDay(day);
 }).then(function (results) {
   console.timeEnd("Total Time");
-  var people = [];
   results.forEach(function (day, index) {
+    var saver = new Saver('tmp/days/'+index+'.json'),
+        people = [];
     day.forEach(function (person) {
       people.push(person);
     });
-    // saver.manualSetData(index, day);
+    saver.manualSetData('people', people);
   });
-  saver.manualSetData('people', people);
 }, function (err) {
   console.log("Error: ", err);
 }).done()
